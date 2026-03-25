94f3fd3e5a38177892886c23e2eda0f0d85e733f
An0nym26s <162511907+An0nym26s@users.noreply.github.com>
2026-03-25 17:36:32 +0100

    Create config.py for environment variable configuration

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# Environment variable configuration for security service keys
+os.environ['VIRUSTOTAL_API_KEY'] = 'your_virustotal_api_key'
+os.environ['OTHER_SECURITY_SERVICE_KEY'] = 'your_other_service_key'
+# Add other keys as needed
